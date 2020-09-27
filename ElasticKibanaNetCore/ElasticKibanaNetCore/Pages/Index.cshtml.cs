70ba1d00afe9d2b47cb62b39eecce69998287fc6
andre.marcondes-ext@viavarejo.com.br <bistomania@live.com>
2020-09-27 18:16:24 -0300

    POC elasticsearch with kibana

---
+++ ElasticKibanaNetCore/ElasticKibanaNetCore/Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ElasticKibanaNetCore.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            try
+            {
+                throw new Exception("Ops, não foi possível prosseguir.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Seu código está bugado!");
+            }
+        }
+    }
+}
